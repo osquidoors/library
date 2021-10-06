13e63912b54a829a743dd88bfd0408fbd3a97c0a
Oscar <oscar@gralacha.cps.bo>
2021-10-06 11:41:44 -0400

    formulario con subida de archivos. Capitulo 7 terrminado

--- src/Controller/Api/BookController.php
+++ src/Controller/Api/BookController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Book;
+use App\Form\Model\BookDto;
 use App\Form\Type\BookFormType;
 use App\Repository\BookRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -10,6 +11,7 @@ use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Request;
+use League\Flysystem\FilesystemOperator;
 
 class BookController extends AbstractFOSRestController {
 
@@ -25,11 +27,19 @@ class BookController extends AbstractFOSRestController {
      * @Rest\Post(path="/book/")
      * @Rest\View(serializerGroups={"book"}, serializerEnableMaxDepthChecks=true)
      */
-    public function book( EntityManagerInterface $em, Request $request) {
-        $book = new Book();
-        $form = $this->createForm(BookFormType::class, $book);
+    public function book( EntityManagerInterface $em, Request $request, FilesystemOperator $defaultStorage) {
+        $bookDto = new BookDto();
+        $form = $this->createForm(BookFormType::class, $bookDto);
         $form->handleRequest( $request );
         if( $form->isSubmitted() && $form->isValid() ) {
+            // print ($bookDto->base64Image); die();
+            $extension = explode('/', mime_content_type($bookDto->base64Image))[1];
+            $data = explode(',', $bookDto->base64Image);
+            $fileName = sprintf('%s.%s', uniqid('book', true), $extension);
+            $defaultStorage->write($fileName, base64_decode($data[1]));
+            $book = new Book();
+            $book->setTitle( $bookDto->title );
+            $book->setImage( $fileName );
             $em->persist($book);
             $em->flush();
             return $book;
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BookRepository::class)
